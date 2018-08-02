1607184fb133258178481f4eee7472e7084630d3
E-Randz <e.randz30@gmail.com>
2018-08-02 18:27:46 +0100

    added page to create a new coffeeshop with image

--- app.js
+++ app.js
@@ -5,23 +5,32 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 
+let coffeeshops = [
+        {name: "Grindsmith", image: 'https://source.unsplash.com/xLQBjknwmzU/400x300'},
+        {name: "Pot Kettle Black", image: 'https://source.unsplash.com/9uxJt-LtqKU/400x300'},
+        {name: "The Department of Coffee and Social Affairs", image: 'https://source.unsplash.com/k_RYBedEvDw/400x300'}
+    ]
+
 app.get("/", function(req, res){
     res.render("landing");
 });
 
 app.get("/coffeeshops", function(req, res){
-    let coffeeshops = [
-            {name: "Grindsmith", image: 'https://source.unsplash.com/xLQBjknwmzU/400x300'},
-            {name: "Pot Kettle Black", image: 'https://source.unsplash.com/9uxJt-LtqKU/400x300'},
-            {name: "The Department of Coffee and Social Affairs", image: 'https://source.unsplash.com/k_RYBedEvDw/400x300'}
-        ]
         res.render("coffeeshops", {coffeeshops: coffeeshops});
 });
 
 app.post("/coffeeshops", function(req,res){
     // get data from form and add to campground array
-    res.send("you hit the post route");
+    let name = req.body.name;
+    let image = req.body.image;
+    let newCoffeeshop = {name: name, image: image};
+    coffeeshops.push(newCoffeeshop);
     // redirect back to campgrounds page
+    res.redirect("/coffeeshops");
+});
+
+app.get("/coffeeshops/new", function(req,res){
+  res.render("new");
 });
 
 var server = app.listen(8080, function(){
